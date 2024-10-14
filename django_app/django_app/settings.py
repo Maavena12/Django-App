de436c1afc93aa11a6b5d29cbacc94b3819938c6
Mariosor12 <marioavena120898@gmail.com>
2024-10-14 17:55:25 -0400

    commit

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL =  os.path.join(BASE_DIR, 'bills', 'static')
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
