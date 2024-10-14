ccf733f1fa9ca7e57dc142451fa90416d74e22d9
Mariosor12 <marioavena120898@gmail.com>
2024-10-14 18:25:20 -0400

    commit

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -22,7 +22,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'bills', 'static'),  # Esto incluye tu carpeta estática en 'bills'  
 ]  
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'  
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SESSION_COOKIE_NAME = 'usuario_id'
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
